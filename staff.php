a0721e177f11f2ed4666c545d46e25608eb47acf
Ladanividhi <167624617+Ladanividhi@users.noreply.github.com>
2025-04-14 17:47:22 +0530

    Delete staff.php

--- staff.php
+++
@@ -1,9 +0,0 @@
-<?php
-    include "connection.php";
-    include "headerstaff.php";
-
-    // echo "<br>Welcome ".$_COOKIE["uname"];
-    // echo "<br>".$_SESSION["uid"];
-
-    include "footerstaff.php";
-?>
