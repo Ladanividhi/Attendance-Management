49fbd50f65a16383de1798f1800034df5e96e8fd
Ladanividhi <167624617+Ladanividhi@users.noreply.github.com>
2025-04-14 17:47:43 +0530

    Delete student.php

--- student.php
+++
@@ -1,9 +0,0 @@
-<?php
-    include "connection.php";
-    include "headerstudent.php";
-
-    // echo "<br>Welcome ".$_COOKIE["sname"];
-    // echo "<br>".$_SESSION["sid"];
-
-    include "footerstudent.php";
-?>
